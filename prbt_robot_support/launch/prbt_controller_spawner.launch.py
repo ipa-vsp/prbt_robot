0fd73651f9dc6d602575e78cdbffa61535f55c5f
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2023-08-23 13:54:01 +0200

    Switch to forward position controller

--- prbt_robot_support/launch/prbt_controller_spawner.launch.py
+++ prbt_robot_support/launch/prbt_controller_spawner.launch.py
@@ -24,10 +24,17 @@ def generate_launch_description():
         arguments=["arm_controller", "--controller-manager", "/controller_manager"],
     )
 
+    forward_position_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["forward_position_controller", "--controller-manager", "/controller_manager"],
+    )
+
 
     return LaunchDescription(
         [
             joint_state_broadcaster_spawner,
-            arm_controller_spawner,
+            # arm_controller_spawner,
+            forward_position_controller_spawner, 
         ],
     )
